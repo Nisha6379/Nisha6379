ebeb3f2ee0b665cb7cc84f51b2f6d092f8a4e6d3
Nisha6379 <144546016+Nisha6379@users.noreply.github.com>
2023-09-14 16:00:56 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,15 +1,31 @@
-# Python program to check if year is a leap year or not
-# To get year (integer input) from the user
-year = int(input("Enter a year: "))
-# divided by 100 means century year (ending with 00)
-# century year divided by 400 is leap year
-if (year % 400 == 0) and (year % 100 == 0):
-    print("{0} is a leap year".format(year))
-# not divided by 100 means not a century year
-# year divided by 4 is a leap year
-elif (year % 4 ==0) and (year % 100 != 0):
-    print("{0} is a leap year".format(year))
-# if not divided by both 400 (century year) and 4 (not century year)
-# year is not leap year
-else:
-    print("{0} is not a leap year".format(year))
+class BankAccount:
+  def __init__(self,account_number,account_holder_name,initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
+    
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance += amount
+      print("Deposited ₹{}. new balance: ₹{}".format(amount,self.__account_balance))
+    else:
+      print("invalid deposit amount.please deposit a positive amount.")
+      
+  def withdraw(self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      print("withdraw ₹{}. new balance: ₹{}".format(amount,self.__account_balance))
+    else:
+      print("invalid withdrawal amount or insufficient balance.")
+      
+  def display_balance(self):
+    print("account balance for {} (account #{}): ₹{}".format(self.__account_holder_name,self.__account_number,self.__account_balance))
+    
+ #create an instance of the BankAccount class   
+account = BankAccount(account_number="123456789",account_holder_name="Sumathi",initial_balance=5000.0)
+
+ #Test deposit and withdrawel functionality
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+account.display_balance()
