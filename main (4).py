d488a5173a4995e79265f26518e60592cd46c153
Nisha6379 <144546016+Nisha6379@users.noreply.github.com>
2023-09-29 13:42:16 +0530

    Update main (4).py

--- main (4).py
+++ main (4).py
@@ -1,29 +1,13 @@
-def linear_search_product(product_list, target_product):
-
-    indices = []
-
-    for i, product in enumerate(product_list):
-
-        if product == target_product:
-
-            indices.append(i)
-
-    return indices
-
-
-
-# Example usage:
-
-products = ["apple", "banana", "apple", "orange", "apple"]
-
-target = "apple"
-
-result = linear_search_product(products, target)
-
-if result:
-
-    print(f"The product '{target}' was found at indices: {result}")
-
-else:
-
-    print(f"The product '{target}' was not found in the list.")
+class player:
+  def play(self):
+    print("the player is playing cricket.")
+class Batsman(player):
+  def play(self):
+    print("the batsman is batting.")
+class Bowler(player):
+  def play(self):
+    print("the bowler is bowling.")
+batsman = Batsman()
+bowler = Bowler()
+batsman.play()
+bowler.play()
